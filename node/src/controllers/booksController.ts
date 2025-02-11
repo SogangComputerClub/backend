3f0b53737563ed5f3e1285b3b280b143b5c895ac
Hans <eodnjs5498@naver.com>
2025-02-11 16:58:00 +0900

    /books -> /book

--- node/src/controllers/booksController.ts
+++ node/src/controllers/booksController.ts
@@ -2,27 +2,6 @@ import 'dotenv/config';
 import { Request, Response } from 'express';
 import { pool } from '../middlewares/db';
 
-// Get All Books
-export const getAllBooks = async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query('SELECT * FROM books');
-        res.json(result.rows);
-    } catch(error){
-        console.error('Error fetching books: ', error);
-        res.status(500).json({error: 'Internal server error'});
-    } 
-};
-
-// Get All Available Books
-export const getAvailableBooks = async(req: Request, res: Response) => {
-    try {
-        const result = await pool.query('SELECT * FROM books WHERE is_available = true');
-        res.json(result.rows);
-    } catch(error){
-        console.error('Error fetching books: ', error);
-        res.status(500).json({error: 'Internal server error'});
-    }
-};
 export const getBooks = async (req: Request, res: Response) => {
     const { available, author } = req.query;
     let query = 'SELECT * FROM books';
--- node/src/routes/books.ts
+++ node/src/routes/books.ts
@@ -9,11 +9,9 @@ export default router;
 */
 
 import express from 'express';
-import { getAllBooks, getAvailableBooks, getBooks } from '../controllers/booksController';
+import { getBooks } from '../controllers/booksController';
 
 const router = express.Router();
-router.get('/allbooks', getAllBooks);
-router.get('/available', getAvailableBooks);
-router.get('/books', getBooks);
+router.get('/book', getBooks);
 
 export default router;
