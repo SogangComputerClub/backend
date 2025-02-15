2efa386af9b4caa56b5869243247648ecb20208f
Kim Myeongseop <81076998+amicus-veritatis@users.noreply.github.com>
2025-02-15 15:07:02 +0900

    Update index.ts

--- node/src/index.ts
+++ node/src/index.ts
@@ -78,7 +78,7 @@ console.log('Generated Swagger Specification:', JSON.stringify(swaggerSpec, null
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // router
-app.use('/api/v1', booksRouter); // /api/v1/allbooks 경로에서 사용
+app.use('/api/v1/library', booksRouter); // /api/v1/allbooks 경로에서 사용
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/protected', protectedRouter);
 // Start the server
