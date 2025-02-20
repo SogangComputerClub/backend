9e8b78c0abcdd1acf4b5d8a07dcc772f60a9ba37
Hans <eodnjs5498@naver.com>
2025-02-20 18:02:35 +0900

    book api 초고 작성 완료

--- node/src/routes/book.ts
+++ node/src/routes/book.ts
@@ -1,8 +1,12 @@
-/** 
+import express from 'express';
+import { getBook, getBookById } from '../controllers/booksController';
+
+
+/**
  * @swagger
  * /api/v1/book:
  *  get:
- *      summary: Retrieve of all books in DB.
+ *      summary: Retrieve all books in DB.
  *      description: Fetches a list of books from the database.
  *      responses:
  *          200:
@@ -11,11 +15,31 @@
  *              description: Internal server error.
 */
 
-import express from 'express';
-import { getBook, getBookById } from '../controllers/booksController';
+/** 
+ * @swagger
+ * /api/v1/book/{id}:
+ *  get:
+ *      summary: Retrieve a book by ID.
+ *      description: Fetches a list of book from the database using its ID.
+ *      parameters:
+ *          in: path
+ *          name: id
+ *          description: The unique ID of the book.
+ *          schema:
+ *              tyle: string
+ *      responses:
+ *          200:
+ *              description: A sucessful response with the requested book.
+ *          404:
+ *              description: Book not found.
+ *          500:
+ *              description: Internal server error.
+*/
 
 const router = express.Router();
+
 router.get('/book', getBook);
+
 router.get('/book/:id', getBookById);
 
 export default router;
